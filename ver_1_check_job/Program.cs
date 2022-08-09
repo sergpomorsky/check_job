c342bb3d62d1c796f176f2f8f2835327bb05fe06
deardaddy <sergpomorsky@gmail.com>
2022-08-09 18:40:36 +0300

    Add_new_array_for_record_new_symbols

--- ver_1_check_job/Program.cs
+++ ver_1_check_job/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: написать программу, которая из имеющегося массива срок формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+// Примеры:
+// ["hello","2","world",":-)"]->["2",":-)"]
+// ["1234","1567","-2","computer science"]->["-2"]
+// ["Russia","Denmark","Kazan"]->[]
+
+string[] texts=new string[4]{"hello","2","world",":-)"};
+int count = 0;
+for (int i = 0; i < texts.Length; i++)
+{
+      if (texts[i].Length <= 3)
+        {
+            count++;
+        }
+}
+string[] result=new string[count];
+void Main(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
